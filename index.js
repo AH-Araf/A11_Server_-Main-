25b4a1fd0a1ea1cf96c7a8273989f147468c3295
Araf <arafathossain.baiust@gmail.com>
2022-11-10 19:56:16 +0600

    data update

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-
 // git add .
 //git commit -m "data update"
 //git push
